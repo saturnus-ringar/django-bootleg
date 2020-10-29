5432ba754b9e460270f4a3df3855ee05a58d2db8
stefan <sandersson@fastmail.fm>
2020-10-29 17:49:27 +0100

    added print_env-command

---
+++ bootleg/management/commands/print_env.py
@@ -0,0 +1,11 @@
+import os
+
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Prints all environment variables'
+
+    def handle(self, *args, **options):
+        for item, value in sorted(os.environ.items()):
+            self.stdout.write('{}: {}'.format(item, value))
