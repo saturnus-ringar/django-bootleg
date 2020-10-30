752966e7be167d580befab7aef8ab5bf073d3346
stefan <sandersson@fastmail.fm>
2020-10-30 17:53:19 +0100

    set django settings at runtime 🥶

--- bootleg/settings.py
+++ bootleg/settings.py
@@ -1,5 +1,8 @@
 from bootleg.conf import settings
 from bootleg.logging import logging
+from bootleg.utils import file_system
+from django.conf import settings as django_settings
+# somewhat ugly to change the django settings at runtime ...nevertheless...
 
 #####################################################
 # django settings
@@ -10,7 +13,6 @@ SITE_ID = 1
 #####################################################
 # django tables 2
 #####################################################
-from bootleg.utils import file_system
 
 DJANGO_TABLES2_TEMPLATE = "django_tables2/bootstrap-responsive.html"
 
@@ -34,7 +36,7 @@ log_dir = settings.log_dir()
 log_level = settings.log_level()
 
 if file_system.is_writable(log_dir):
-    LOGGING = {
+    django_settings.LOGGING = {
         'version': 1,
         'disable_existing_loggers': False,
         'formatters': {
@@ -66,7 +68,7 @@ if file_system.is_writable(log_dir):
     }
 
     if settings.log_sql():
-        LOGGING["loggers"]["django.db.backends"] = {
+        django_settings.LOGGING["loggers"]["django.db.backends"] = {
             'level': 'DEBUG', # always debug on this one
             'handlers': ['sql']
         }
