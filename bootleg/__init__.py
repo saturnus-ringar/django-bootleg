a69a31211d49ff1f0ed1f85da62d47b0082f61de
stefan <sandersson@fastmail.fm>
2020-11-02 20:03:41 +0100

    fix?!?!?!

--- bootleg/__init__.py
+++ bootleg/__init__.py
@@ -20,5 +20,5 @@ def setup():
     if bootleg_settings.ADD_BUILTINS:
         logging.add_builtins()
 
-    if bootleg_settings.PRINT_AT_STARTUP:
-        bootstrap.startup_print()
+    #if bootleg_settings.PRINT_AT_STARTUP:
+    #    bootstrap.startup_print()
