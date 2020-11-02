75798ca63d6e5c9a1e0623663bf9ab37b48b94d5
stefan <sandersson@fastmail.fm>
2020-11-02 20:25:43 +0100

    some intentionally broken code in here to test django errors

--- bootleg/__init__.py
+++ bootleg/__init__.py
@@ -1,14 +1,9 @@
-#from bootleg import bootstrap
-#from django.utils.version import get_version
-
-VERSION = (0, 0, 13, 'beta', 0)
-#__version__ = get_version(VERSION)
-__version__ = "0.11o"
-
+__version__ = "0.12o"
 default_app_config = "bootleg.apps.BootlegConfig"
 
 
 def setup():
+    from bootleg import bootstrap
     from bootleg.conf import bootleg_settings
     from bootleg.logging import logging
     from bootleg.settings import set_django_settings
@@ -20,5 +15,5 @@ def setup():
     if bootleg_settings.ADD_BUILTINS:
         logging.add_builtins()
 
-    #if bootleg_settings.PRINT_AT_STARTUP:
-    #    bootstrap.startup_print()
+    if bootleg_settings.PRINT_AT_STARTUP:
+        bootstrap.startup_print()
