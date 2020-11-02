8b9e0c5703c4b48ff2bd24d3308345ddb4c858b8
stefan <sandersson@fastmail.fm>
2020-11-02 15:57:22 +0100

    code cleanup

--- bootleg/urls_apps.py
+++ bootleg/urls_apps.py
@@ -10,13 +10,10 @@ urlpatterns = [
     # django admin
     #######################################
     path('django-admin/', admin.site.urls),
-
-
 ]
 
 if settings.DEBUG:
     import debug_toolbar
-
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
