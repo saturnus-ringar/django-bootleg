847edac12125274a8d28dbf344987c21107d0280
stefan <sandersson@fastmail.fm>
2020-11-02 16:09:11 +0100

    log 400, 403 and 404

--- bootleg/views/errors.py
+++ bootleg/views/errors.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from bootleg.conf import bootleg_settings
+from bootleg.logging import logging
 
 
 def error_handler(request, template, status_code):
@@ -9,15 +10,23 @@ def error_handler(request, template, status_code):
     return response
 
 
+def log(status_code, request):
+    logging.get_logger("errors/%s" % str(status_code)).error("%s on the URL: %s" %
+                                                             (str(status_code), request.get_full_path()))
+
+
 def handler400(request, exception):
+    log(400, request)
     return error_handler(request, bootleg_settings.ERROR_400_TEMPLATE, 400)
 
 
 def handler403(request, exception):
+    log(403, request)
     return error_handler(request, bootleg_settings.ERROR_403_TEMPLATE, 403)
 
 
 def handler404(request, exception):
+    log(404, request)
     return error_handler(request, bootleg_settings.ERROR_404_TEMPLATE, 404)
 
 
