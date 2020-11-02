9f9e825820de68fd11af82d4305fe9abcae0d1eb
stefan <sandersson@fastmail.fm>
2020-11-02 15:13:02 +0100

    print custom settings at startup

--- bootleg/bootstrap.py
+++ bootleg/bootstrap.py
@@ -7,6 +7,10 @@ from bootleg.conf import bootleg_settings
 from bootleg.utils import models
 
 
+def print_setting(text, value):
+    print(Fore.LIGHTYELLOW_EX + text.ljust(40) + "\t" + str(value))
+
+
 def startup_print():
     print(Fore.LIGHTBLUE_EX + "*********************************************************************")
     print("Running django bootleg version: %s" % bootleg.__version__)
@@ -15,19 +19,19 @@ def startup_print():
     else:
         print(Fore.LIGHTYELLOW_EX + "We're NOT running in " + Fore.LIGHTBLUE_EX + "debug")
 
-    print(Fore.LIGHTYELLOW_EX + "Database backend:\t" + connection.vendor)
-    print("Using database:\t\t" + connection.settings_dict['NAME'])
-    print("Log-dir:\t\t" + bootleg_settings.LOG_DIR)
-    print("Log level:\t\t" + bootleg_settings.LOG_LEVEL)
-    print("Django log level:\t" + bootleg_settings.DJANGO_LOG_LEVEL)
-    print("Static root:\t\t" + getattr(settings, "STATIC_ROOT"))
-    print("Static url:\t\t" + getattr(settings, "STATIC_URL"))
-    print("Media root:\t\t" + getattr(settings, "MEDIA_ROOT"))
-    print("Media url:\t\t" + getattr(settings, "MEDIA_URL"))
+    print_setting("Database backend", connection.vendor)
+    print_setting("Database", connection.settings_dict['NAME'])
+    print_setting("Log-dir", bootleg_settings.LOG_DIR)
+    print_setting("Log level", bootleg_settings.LOG_LEVEL)
+    print_setting("Django log level",  bootleg_settings.DJANGO_LOG_LEVEL)
+    print_setting("Static root", getattr(settings, "STATIC_ROOT"))
+    print_setting("Static url", getattr(settings, "STATIC_URL"))
+    print_setting("Media root", getattr(settings, "MEDIA_ROOT"))
+    print_setting("Media url", getattr(settings, "MEDIA_URL"))
 
     editable_models = models.get_editable_models()
     if editable_models:
-        print("Editable models:\t" + str(models.get_editable_models()))
+        print_setting("Editable models", str(models.get_editable_models()))
 
     if bootleg_settings.LOG_SQL:
         print(Fore.GREEN + "* Logging SQL")
@@ -38,5 +42,12 @@ def startup_print():
     if bootleg_settings.STORE_DJANGO_LOG_EXCEPTIONS:
         print(Fore.GREEN + "* Storing Django log exceptions")
 
+    custom_settings_to_print = getattr(settings, "SETTINGS_TO_PRINT")
+    if custom_settings_to_print:
+        print(Fore.LIGHTBLUE_EX + "Custom settings")
+        for setting, value in custom_settings_to_print.items():
+            print_setting(setting, value)
+
     print(Fore.LIGHTBLUE_EX + "*********************************************************************")
     print(Style.RESET_ALL)
+
