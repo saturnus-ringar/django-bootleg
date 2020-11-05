42cb3fbf292ad3348a637b011b4a46586d180856
stefan <sandersson@fastmail.fm>
2020-11-05 17:23:16 +0100

    deploy-info-fixes

--- bootleg/system/git.py
+++ bootleg/system/git.py
@@ -14,7 +14,10 @@ class GitData:
 def get_github_url(repo):
     parsed = giturlparse.parse(repo.remotes.origin.url)
     split = parsed.urls["https"].split("@")
-    return "https://" + split[1].replace(".git", "")
+    if len(split) > 1:
+        return "https://" + split[1].replace(".git", "")
+    else:
+        return split[0]
 
 
 def get_git_repo():
