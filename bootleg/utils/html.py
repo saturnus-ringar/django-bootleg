626f31eaa7a6c5e0ed1caa2a6e64108d2075a817
stefan <sandersson@fastmail.fm>
2020-11-30 18:41:59 +0100

    fixed badges in navbar

--- bootleg/utils/html.py
+++ bootleg/utils/html.py
@@ -98,7 +98,6 @@ def get_nav_item(url, text, count=None, target_blank=False):
     html = '<li class ="nav-item">\n'
     html += '<a class="nav-link" href="%s"%s>%s\n' % (url, target, text)
     if count:
-        html += '<span class ="badge badge-pill badge-danger navbar-badge">%s</span>' \
-                % count
+        html += '<sup><span class ="badge badge-pill badge-danger">%s</span></sup>' % count
     html += '</a>\n'
     return html
