0d11fc5799e7f60322ca79e0613931fa52ca6f3f
stefan <sandersson@fastmail.fm>
2020-11-30 21:41:56 +0100

    fixed user check

--- bootleg/__init__.py
+++ bootleg/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.91o"
+__version__ = "0.92o"
 default_app_config = "bootleg.apps.BootlegConfig"
 
 
--- bootleg/management/commands/base.py
+++ bootleg/management/commands/base.py
@@ -11,8 +11,8 @@ class UserError(Exception):
 class UserRequirementCommand(BaseCommand):
 
     def __init__(self, stdout=None, stderr=None, no_color=False, force_color=False):
-        if not nix.is_current_user(bootleg_settings.main_user()):
-            raise UserError("This command must be run by the user: %s" % bootleg_settings.main_user())
+        if not nix.is_current_user(bootleg_settings.MAIN_USER):
+            raise UserError("This command must be run by the user: %s" % bootleg_settings.MAIN_USER)
         super().__init__(stdout, stderr, no_color, force_color)
 
     def handle(self, *args, **options):
