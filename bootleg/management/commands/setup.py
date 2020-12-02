9eb8c87342d8e2a466571c31624dacb04bea8b84
stefan <sandersson@fastmail.fm>
2020-12-02 10:14:52 +0100

    dir setup-fixes

--- bootleg/management/commands/setup.py
+++ bootleg/management/commands/setup.py
@@ -14,10 +14,11 @@ class Command(BaseCommand):
         self.logger.info("Fixing directories")
         self.fix_dir(settings.LOG_DIR)
         self.fix_dir(settings.LOG_DIR + "cron")
-        if getattr(settings, "STATIC_ROOT"):
-            self.fix_dir(settings.STATIC_ROOT)
-        if getattr(settings, "MEDIA_ROOT"):
-            self.fix_dir(settings.MEDIA_ROOT)
+        if settings.DEBUG is False:
+            if getattr(settings, "STATIC_ROOT"):
+                self.fix_dir(settings.STATIC_ROOT)
+            if getattr(settings, "MEDIA_ROOT"):
+                self.fix_dir(settings.MEDIA_ROOT)
         self.fix_dir(get_virtual_env_path())
         self.fix_dir(settings.BASE_DIR)
         self.logger.info("Done with setup")
@@ -30,52 +31,3 @@ class Command(BaseCommand):
         run_command(["chmod", "-R", "770", directory])
         run_command(["chown", "-R", main_user + ":" + user_group, directory])
         run_command(["chmod", "-R", "g+s", directory])
-
-
-'''
-#!/bin/bash
-mkdir -p /var/log/gigguide/
-chmod -R 770 /var/log/gigguide/
-chown -R gigguide:www-data /var/log/gigguide/
-chmod -R g+s /var/log/gigguide/
-
-mkdir -p /var/log/gigguide/cron/
-chmod -R 770 /var/log/gigguide/cron/
-chown -R gigguide:www-data /var/log/gigguide/cron/
-chmod -R g+s /var/log/gigguide/cron/
-
-mkdir -p /var/www/gigguide/static/
-chmod -R 770 /var/www/gigguide/static/
-chown -R gigguide:www-data /var/www/gigguide/static/
-chmod -R g+s /var/www/gigguide/static/
-
-mkdir -p /var/www/gigguide/media/
-chmod -R 770 /var/www/gigguide/media/
-chown -R gigguide:www-data /var/www/gigguide/media/
-chmod -R g+s /var/www/gigguide/media/
-
-chmod -R 770 /home/gigguide/env/
-chown -R gigguide:www-data /home/gigguide/env/
-chmod -R g+s /home/gigguide/env/
-
-chmod -R 770 /home/gigguide/gigguide/
-chown -R gigguide:www-data /home/gigguide/gigguide/
-chmod -R g+s /home/gigguide/gigguide/
-
-mkdir -p /home/gigguide/backup/
-chmod -R 770 /home/gigguide/backup/
-chown -R gigguide:gigguide /home/gigguide/backup/
-chmod -R g+s /home/gigguide/backup/
-
-touch /home/gigguide/backup/gigguide.sql
-chmod 770 /home/gigguide/backup/gigguide.sql
-chown gigguide:gigguide /home/gigguide/backup/gigguide.sql
-
-mkdir -p /var/log/gunicorn/
-chown -R www:www-data /var/log/gunicorn/
-chmod -R g+s /var/log/gunicorn/
-
-mkdir -p /var/spacy/
-chmod -R 770 /var/spacy/
-chown -R gigguide:gigguide /var/spacy/
-'''
