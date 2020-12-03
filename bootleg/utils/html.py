f2d9a44e358e0c2ea587a9363d08f338b1f424a2
stefan <sandersson@fastmail.fm>
2020-12-03 10:46:50 +0100

    code format fixes

--- bootleg/utils/html.py
+++ bootleg/utils/html.py
@@ -8,6 +8,7 @@ from django.utils.translation import ugettext as _
 def strip_tags(html):
     return re.sub('<[^<]+?>', '', html)
 
+
 def get_default_table_class_string():
     return "table table-striped table-responsive table-hover w-100 d-block d-md-table"
 
