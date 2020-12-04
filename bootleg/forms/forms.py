a95cf865cb0be81e26f3c623b3f3d2bd4b1ab77f
stefan <sandersson@fastmail.fm>
2020-12-04 20:47:33 +0100

    no monkey patching here... .. ... ....

--- bootleg/forms/forms.py
+++ bootleg/forms/forms.py
@@ -1,6 +1,4 @@
 from crispy_forms.helper import FormHelper
-from django_enumfield.forms.fields import EnumChoiceField
-
 from bootleg.forms.base import METHOD_GET, BaseForm
 from django.forms import CharField, modelform_factory, SelectMultiple, Select
 from django.utils.translation import ugettext as _
@@ -49,18 +47,3 @@ class GenericModelSearchForm(BaseForm):
             self.add_attribute(self.fields["q"], "placeholder", _("Search"))
             self.add_class(self.fields["q"], "generic-autocomplete")
             self.add_class(self.fields["q"], "form-control")
-
-
-# monkey patch EnumChoiceField so it has an emtpy label
-def enum_choice_field_init(self, enum, **kwargs):
-    choices = [("", "--------------")] + enum.choices(blank=not kwargs.get("required", True))
-    kwargs["choices"] = choices
-    kwargs["initial"] = ""
-    kwargs.setdefault(
-        "choices", choices
-    )
-    kwargs.setdefault("coerce", int)
-    super(EnumChoiceField, self).__init__(**kwargs)
-    self.enum = enum
-
-EnumChoiceField.__init__ = enum_choice_field_init
