3dff7973e82f5854cfc42e7bf6cffca66f21e1ad
stefan <sandersson@fastmail.fm>
2020-12-04 14:20:37 +0100

    added sudo

--- bootleg/management/commands/deploy.py
+++ bootleg/management/commands/deploy.py
@@ -31,9 +31,9 @@ class Command(UserRequirementCommand):
         management.call_command("collectstatic", interactive=False)
         if not options["soft"]:
             if env.is_apache_from_cli():
-                run_command(["systemctl", "restart", "apache2"])
+                run_command(["sudo", "systemctl", "restart", "apache2"])
             elif env.is_gunicorn_from_cli():
-                run_command(["systemctl", "restart", "gunicorn.socket"])
+                run_command(["sudo", "systemctl", "restart", "gunicorn.socket"])
             else:
                 raise ValueError("Could not determine which server type this is running on. Can't restart.")
 
