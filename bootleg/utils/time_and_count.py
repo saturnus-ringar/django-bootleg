faafead0876ed9ce80848381f26b9eee6a8d3de8
stefan <sandersson@fastmail.fm>
2020-12-13 12:52:08 +0100

    improved logging

--- bootleg/utils/time_and_count.py
+++ bootleg/utils/time_and_count.py
@@ -2,6 +2,7 @@ import datetime
 import time
 
 import humanize
+from django.contrib.humanize.templatetags.humanize import intcomma
 from django.utils.translation import ugettext as _
 
 
@@ -26,19 +27,20 @@ class TimeAndCount:
         average_time = elapsed_time / self.tac_number_run
         expected_time_left = int(round((self.tac_total_count - self.tac_number_run) * average_time, 0))
         if self.tac_number_run % self.print_every_th == 0:
-            self.print_tac_message(percent_done, expected_time_left, elapsed_time)
+            self.print_tac_message(percent_done, expected_time_left, elapsed_time, average_time)
 
-    def print_tac_message(self, percent_done, expected_time_left, elapsed_time):
+    def print_tac_message(self, percent_done, expected_time_left, elapsed_time, average_time):
         delta_running_time = datetime.timedelta(seconds=elapsed_time)
         msg = _("Done: ")
-        msg += str(self.tac_number_run) + "/" + str(self.tac_total_count)
+        msg += intcomma(self.tac_number_run) + "/" + intcomma(self.tac_total_count)
         msg += " (" + str(percent_done) + "%)"
         if expected_time_left > 0:
             msg += " - " + _("Expected to be done in") + ": " + humanize.precisedelta(expected_time_left,
                                                                                    minimum_unit="minutes", format="%0.0f")
         msg += " - " + _("Running time") + ": " + humanize.precisedelta(delta_running_time, minimum_unit="minutes",
                                                                         format="%0.0f")
-
+        # hmmm... weird calculations here
+        msg += " - " + _("Average time per entry") + ": " + str(round(average_time, 5)) + "s"
         if self.logger:
             self.logger.info(msg)
         else:
