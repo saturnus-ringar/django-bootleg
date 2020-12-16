4cda3b4cc6a6093c3bba06f03a7b13c97755dec5
stefan <sandersson@fastmail.fm>
2020-12-16 20:00:57 +0100

    search-indexing tweaks and additions

--- bootleg/management/commands/index.py
+++ bootleg/management/commands/index.py
@@ -1,9 +1,7 @@
-from django.contrib.humanize.templatetags.humanize import intcomma
 from django.core.management import BaseCommand
-from django_elasticsearch_dsl.registries import registry
 
 from bootleg.logging import logging
-from bootleg.utils.models import lazy_bulk_fetch
+from bootleg.utils.search import SearchIndexer
 from bootleg.utils.time_and_count import TimeAndCount
 
 
@@ -16,18 +14,4 @@ class Command(BaseCommand, TimeAndCount):
         parser.add_argument("-p", "--parallel", action="store_true", dest="parallel")
 
     def handle(self, *args, **options):
-        batch_size = options["batch"]
-        parallel = options["parallel"]
-        for doc in registry.get_documents():
-            count = doc.django.model.objects.count()
-            # init time and count
-            self.init_tac(count, self.logger, print_every_th=1000)
-            self.logger.info("Indexing model: [%s]. Batch size: [%s]. Parallel: [%s]. Total count: [%s]"
-                             % (doc.django.model.__name__, batch_size, parallel, intcomma(count)))
-            fetcher = lazy_bulk_fetch(batch_size, count,
-                                      lambda: doc.django.model.objects.order_by("id"))
-            for batch in fetcher:
-                self.logger.info("Indexing: [%s]. Starting at ID: [%s]"
-                                 % (doc.django.model.__name__, batch.first().id))
-                doc().update(batch, parallel=parallel)
-                self.update_tac(number=batch_size)
+        SearchIndexer().run(options["batch"], options["parallel"])
--- bootleg/utils/search.py
+++ bootleg/utils/search.py
@@ -1,8 +1,15 @@
+from django.contrib.humanize.templatetags.humanize import intcomma
+from django_elasticsearch_dsl.registries import registry
+
+from bootleg.logging import logging
 from elasticsearch_dsl import Q
 
 
-# an initial start ... to be ... improved
+from bootleg.utils.time_and_count import TimeAndCount
+
+
 class QueryBuilder:
+    # an initial start ... to be ... improved
 
     def __init__(self, model, query):
         self.model = model
@@ -18,6 +25,35 @@ class QueryBuilder:
                 filter = f
 
         return filter
+
+
+class SearchIndexer(TimeAndCount):
+    logger = logging.get_logger("search_index")
+    batch_size = 50
+
+    def run(self, batch_size=None, parallel=False):
+        from bootleg.utils.models import lazy_bulk_fetch
+        batch_size = batch_size or self.batch_size
+        parallel = parallel
+        for doc in registry.get_documents():
+            count = doc.django.model.objects.count()
+            # init time and count
+            start_id = 0 or self.get_start_id(doc)
+            self.init_tac(count, self.logger, print_every_th=1000, start_count=start_id)
+            self.logger.info("Indexing model: [%s]. Batch size: [%s]. Parallel: [%s]. Total count: [%s]"
+                             % (doc.django.model.__name__, batch_size, parallel, intcomma(count)))
+            fetcher = lazy_bulk_fetch(batch_size, count,
+                                      lambda: doc.django.model.objects.order_by("id"), start=start_id)
+            for batch in fetcher:
+                self.logger.info("Indexing: [%s]. Starting at ID: [%s]"
+                                 % (doc.django.model.__name__, batch.first().id))
+                doc().update(batch, parallel=parallel)
+                self.update_tac(number=batch_size)
+
+    def get_start_id(self, doc):
+        if hasattr(doc, "get_start_index_id"):
+            return doc.get_start_index_id()
+        return None
 
 
 def get_document_by_index(index):
