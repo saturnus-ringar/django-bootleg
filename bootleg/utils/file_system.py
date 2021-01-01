773d264feb44350c8a84c96e3829f2a69394dd02
saturnus-ringar <sandersson@fastmail.fm>
2021-01-01 20:44:50 +0100

    sort files

--- bootleg/utils/file_system.py
+++ bootleg/utils/file_system.py
@@ -2,12 +2,13 @@ import os
 from os import path
 
 
-def get_files_recursively(dir):
+def get_files_recursively(dir, files_to_skip=[]):
     if not path.exists(dir):
         raise OSError("Dir: %s doesn't exist." % dir)
     ret_files = []
     for subdir, dirs, files in os.walk(dir):
-        for file in files:
-            ret_files.append(os.path.join(subdir, file))
+        for file in sorted(files):
+            if file not in files_to_skip:
+                ret_files.append(os.path.join(subdir, file))
 
     return ret_files
