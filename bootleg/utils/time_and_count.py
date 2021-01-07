c40d95af65409620bf4d6db6696b4ecab167bb7a
saturnus-ringar <sandersson@fastmail.fm>
2021-01-07 04:54:35 +0100

    fixed time and counts

--- bootleg/utils/time_and_count.py
+++ bootleg/utils/time_and_count.py
@@ -15,25 +15,53 @@ class TimeAndCount:
     tac_total_count = 0
     logger = None
 
-    def init_tac(self, total_count, logger=None, print_every_th=10, start_count=None, identifier=None):
+    def init_tac(self, total_count, logger=None, print_every_th=10, start_count=None, identifier=None,
+                 minimum_print_count=None):
+        if not start_count:
+            self.tac_start_time = time.time()
+        else:
+            self.tac_start_time = None
         self.print_every_th = print_every_th
         self.start_count = start_count
         self.identifier = identifier
         self.tac_total_count = total_count
-        self.tac_start_time = time.time()
         self.average_time_per_entry = None
+        self.minimum_print_count = minimum_print_count
+        self.reset_count = None
         self.tac_message = ""
         if logger:
             self.logger = logger
 
     def update_tac(self, number=1, message=None):
         self.tac_number_run += number
-        if self.tac_number_run % self.print_every_th == 0:
+        if not self.tac_start_time and self.tac_number_run == self.start_count:
+            self.tac_start_time = time.time()
+        if self.should_process_tac():
             percent_done = round((self.tac_number_run / self.tac_total_count) * 100, 1)
             elapsed_time = time.time() - self.tac_start_time
-            average_time = elapsed_time / self.tac_number_run
+            average_time = elapsed_time / (self.tac_number_run - self.get_count_reduce())
             expected_time_left = int(round((self.tac_total_count - self.tac_number_run) * average_time, 0))
             self.print_tac_message(percent_done, expected_time_left, elapsed_time, average_time, message=message)
+
+    def reset_tac(self):
+        self.tac_start_time = time.time()
+        self.reset_count = self.tac_number_run
+
+    def get_count_reduce(self):
+        if self.reset_count:
+            return self.reset_count
+        if self.start_count:
+            return self.start_count
+        return 0
+
+    def should_process_tac(self):
+        if self.minimum_print_count and (self.tac_number_run < self.minimum_print_count):
+            return False
+
+        if self.tac_number_run % self.print_every_th == 0:
+            return True
+
+        return False
 
     def print_tac_message(self, percent_done, expected_time_left, elapsed_time, average_time, message=None):
         delta_running_time = datetime.timedelta(seconds=elapsed_time)
@@ -48,7 +76,7 @@ class TimeAndCount:
         msg += " - " + _("Running time") + ": " + humanize.precisedelta(delta_running_time, minimum_unit="minutes",
                                                                         format="%0.0f")
         msg += " - " + _("Avg. time/row") + ": " + str(round(average_time, 5)) + "s"
-        msg += " - " + _("Rows/h") + ": " + intcomma(int(100 / average_time * 60 * 60))
+        msg += " - " + _("Rows/h") + ": " + intcomma(int(100 / average_time * 60))
         msg += " - " + _("Memory usage") + ": " + get_humanized_memory_usage()
 
         if self.identifier:
