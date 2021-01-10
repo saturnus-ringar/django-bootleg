0346887649825d199fb5f14895c5571eeac52611
saturnus-ringar <sandersson@fastmail.fm>
2021-01-10 22:30:17 +0100

    removed duplicated setting

--- bootleg/conf/settings.py
+++ bootleg/conf/settings.py
@@ -82,7 +82,6 @@ class Settings(Singleton):
         self.add_setting("LOG_DATE_FORMAT",  "%Y-%m-%d %H:%M:%S")
         self.add_setting("LOG_TO_STDOUT", True)
         self.add_setting("LOG_SQL", False)
-        self.add_setting("LOG_SQL", False)
         self.add_setting("LOG_LEVEL", "INFO")
         self.add_setting("LOG_MODEL_SAVES", False)
         self.add_setting("DJANGO_LOG_LEVEL", get_debug_settings_value("ERROR", "INFO"))
